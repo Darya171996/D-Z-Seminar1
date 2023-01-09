5f8417383c50c9248e1d974d97d8736e0f0794d4
PetrikovaDarya <moskov.ru@mail.ru>
2023-01-09 15:33:55 +0400

    Задача4

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -13,13 +13,6 @@ int num3 = Convert.ToInt32(Console.ReadLine());
 
 int max = num1;
 
-if(num2 > max) max = num2;
+if (num2 > max) max = num2;
 if (num3 > max) max = num3;
-Console.WriteLine(max);
-
-
-
-
-
-
-
+Console.WriteLine($"max {max}");
